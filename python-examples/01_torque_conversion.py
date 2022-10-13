096be35e02aeb1b754172e1587eb8256a33a9218
omerbe <89922839+omerbe@users.noreply.github.com>
2022-10-13 10:48:45 -0400

    Update 01_torque_conversion.py

--- python-examples/01_torque_conversion.py
+++ python-examples/01_torque_conversion.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import time
 
@@ -39,8 +40,10 @@ def compute_motor_torques(Tx, Ty, alpha):
     # CODE 
     # GOES 
     # HERE
-
-    return 0, 0, 0
+    T1 = -(1/3)*((2*Ty)/np.cos(alpha))
+    T2 = (1/3)*((1/np.cos(alpha))*(-np.sqrt(3)*Tx+Ty))
+    T3 = (1/3)*((1/np.cos(alpha))*(np.sqrt(3)*Tx+Ty))
+    return T1, T2, T3
 
 if __name__ == "__main__":
     start = time.time()
@@ -49,17 +52,22 @@ if __name__ == "__main__":
     T1_array = []
     T2_array = []
     T3_array = []
-
-    while(t < EXEC_TIME):
+    last_timestop = 0;
+    while((time.time()-start) < EXEC_TIME):
         # Runs @200Hz
-
-        Tx = 3.0 * np.sin(t)
-        Ty = 2.0 * np.cos(t)
-
-        # Compute Motor Torques
-        T1, T2, T3 = compute_motor_torques(Tx, Ty, ALPHA)
-
-        # Append your computed motor torques to their corresponding arrays
-
+        check = time.time() - last_timestop
+        if check >= DT:
+            last_timestop = time.time()
+            Tx = 3.0 * np.sin(t)
+            Ty = 2.0 * np.cos(t)
     
+            # Compute Motor Torques
+            T1, T2, T3 = compute_motor_torques(Tx, Ty, ALPHA)
+            T1_array.append(T1)
+            T2_array.append(T2)
+            T3_array.append(T3)
+            # Append your computed motor torques to their corresponding arrays
+            
+
+    np.savetxt('torques_list.csv',(T1_array,T2_array,T3_array), delimiter=",")
     # Save your arrays as a .csv file
