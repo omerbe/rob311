1598904c3779bf858b0c06b85e120d5e78eeadf1
omerbe <89922839+omerbe@users.noreply.github.com>
2022-10-13 00:43:49 -0400

    Update 00_loop_execution.py

--- python-examples/00_loop_execution.py
+++ python-examples/00_loop_execution.py
@@ -8,7 +8,15 @@ DT = 1/FREQ
 if __name__ == "__main__":
     start = time.time()
     t = 0.0
-
+    x=0
+    last_timestop = 0
+    while (time.time() - start) < 5:
+        printcheck = time.time() - last_timestop
+        if printcheck >= DT:
+            last_timestop = time.time()
+            print("ROB311 @UM-ROBOTICS")
+            x +=1
+    print (x)
     # Print "ROB311 @UM-ROBOTICS" for 5 seconds @200Hz
 
     # YOUR
